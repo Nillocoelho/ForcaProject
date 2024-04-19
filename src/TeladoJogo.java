6a10277c6f271d84f87c778132bba90160cdc600
Danillo <coelho.danillo@academico.ifpb.edu.br>
2024-04-19 10:49:17 -0300

    Sua mensagem de commit aqui

--- src/TeladoJogo.java
+++ src/TeladoJogo.java
@@ -36,10 +36,8 @@ public class TeladoJogo {
 	private JLabel label_8;
 	private JogoDaForca jogo;
 	private int contador;
-	private int contagem;
+	private int start;
 	private JLabel label_9;
-	private JTextPane textPane;
-
 	/**
 	 * Launch the application.
 	 */
@@ -90,7 +88,7 @@ public class TeladoJogo {
 				label_5.setText(jogo.getPalavraAdivinhada());
 				label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/6.png")));
 				label_8.setText("0");
-				contagem = 0;
+				start ++;
 				
 			}
 		});
@@ -140,56 +138,54 @@ public class TeladoJogo {
 		button_1.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (contador > 0) {
-					String texto = textField.getText();
-					if (texto.length() == 1) {
-						System.out.println(letras);
-						for(int i = 0; i < letras.size(); i++) {
-							if(texto != letras.get(i)){
-								System.out.println("Teste");
-								letras.add(texto);
-						try {
-							ArrayList<Integer> ocorrencias = jogo.getOcorrencias(texto);
-							if (!ocorrencias.isEmpty()) {
-								label_9.setText("voce acertou a letra =" + " " + texto);
-								label_5.setText(jogo.getPalavraAdivinhada());
+				if (start != 0) {
+					if (contador > 0) {
+						String texto = textField.getText();
+						if (texto.length() == 1) {
+							letras.add(texto);
+							if(texto != jogo.getPalavraAdivinhada()){
+								try {
+									ArrayList<Integer> ocorrencias = jogo.getOcorrencias(texto);
+									if (!ocorrencias.isEmpty()) {
+										label_9.setText("voce acertou a letra =" + " " + texto);
+										label_5.setText(jogo.getPalavraAdivinhada());
+									}
+									else {
+										start++;
+										label_9.setText("voce errou a letra = " +  " " + texto);
+										label_8.setText("" + start);
+										switch (start) {
+										case 1:
+											label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/5.png")));
+											break;
+										case 2:
+											label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/4.png")));
+											break;
+										case 3:
+											label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/3.png")));
+											break;
+										case 4:
+											label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/2.png")));
+											break;
+										case 5:
+											label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/1.png")));
+											break;
+										case 6:
+											label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/0.png")));
+											JOptionPane.showMessageDialog(null, "Fim de jogo", "Alerta", JOptionPane.WARNING_MESSAGE);
+											label_5.setText(jogo.getPalavra());
+											button_1.setEnabled(false);
+											break;
+										}
+									}
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								}
 							}
 							else {
-								contagem++;
-								label_9.setText("voce errou a letra = " +  " " + texto);
-								label_8.setText("" + contagem);
-								switch (contagem) {
-							    case 1:
-							    	label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/5.png")));
-							        break;
-							    case 2:
-							    	label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/4.png")));
-							        break;
-							    case 3:
-							    	label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/3.png")));
-							        break;
-							    case 4:
-							    	label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/2.png")));
-							    	break;
-							    case 5:
-							    	label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/1.png")));
-							    	break;
-							    case 6:
-							    	label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/0.png")));
-							    	JOptionPane.showMessageDialog(null, "Fim de jogo", "Alerta", JOptionPane.WARNING_MESSAGE);
-							    	label_5.setText(jogo.getPalavra());
-							    	button_1.setEnabled(false);
-							    	break;
+								JOptionPane.showMessageDialog(null, "Você já adicionou essa letra antes", "Alerta", JOptionPane.WARNING_MESSAGE);
 							}
-							}
-						} catch (Exception ex) {
-							ex.printStackTrace();
 						}
-					}
-						else {
-							JOptionPane.showMessageDialog(null, "Você já adicionou essa letra antes", "Alerta", JOptionPane.WARNING_MESSAGE);
-						}
-					}
 					}
 					else {
 						JOptionPane.showMessageDialog(null, "Você adicionou mais de uma letra", "Alerta", JOptionPane.WARNING_MESSAGE);
@@ -203,47 +199,47 @@ public class TeladoJogo {
 
 		button_1.setBounds(176, 88, 89, 23);
 		frame.getContentPane().add(button_1);
-		
+
 		label_2 = new JLabel();
 		label_2.setFont(new Font("Times New Roman", Font.PLAIN, 13));
 		label_2.setBounds(78, 23, 195, 66);
 		frame.getContentPane().add(label_2);
-		
+
 		label_3 = new JLabel("Forca:");
 		label_3.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		label_3.setBounds(294, 45, 89, 17);
 		frame.getContentPane().add(label_3);
-		
+
 		label_4 = new JLabel("Palavra:");
 		label_4.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		label_4.setBounds(29, 134, 66, 14);
 		frame.getContentPane().add(label_4);
-		
+
 		label_5 = new JLabel("");
 		label_5.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		label_5.setBounds(105, 122, 160, 35);
 		frame.getContentPane().add(label_5);
-		
+
 		label_6 = new JLabel("Boneco");
 		label_6.setIcon(new ImageIcon(TeladoJogo.class.getResource("/imagens/0.png")));
 		label_6.setBounds(352, 57, 152, 158);
 		frame.getContentPane().add(label_6);
-		
+
 		label_7 = new JLabel("No de tentativas:");
 		label_7.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		label_7.setBounds(294, 11, 132, 23);
 		frame.getContentPane().add(label_7);
-		
+
 		label_8 = new JLabel("Null");
 		label_8.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		label_8.setForeground(new Color(255, 0, 0));
 		label_8.setBounds(438, 12, 66, 20);
 		frame.getContentPane().add(label_8);
-		
+
 		label_9 = new JLabel("");
 		label_9.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		label_9.setBounds(29, 159, 208, 29);
 		frame.getContentPane().add(label_9);
-		
-	}
+
+}
 }
